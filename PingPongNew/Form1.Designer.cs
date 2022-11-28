402c2220ee7fa29ef1f2818118fe0db85d110b46
Abat <119326883+Abat003@users.noreply.github.com>
2022-11-28 19:05:06 +0500

    files added

---
+++ PingPongNew/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PingPongNew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.racket);
+            this.panel1.Controls.Add(this.ball);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(51, 57);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(38, 32);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.racket.Location = new System.Drawing.Point(294, 392);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(186, 26);
+            this.racket.TabIndex = 1;
+            this.racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PingPongNew/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongNew
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 8;
+        public int speed_top = 8;
+        public int point = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            racket.Top = panel1.Bottom - (panel1.Bottom / 10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X - (racket.Width / 2);
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+
+            if (ball.Bottom>=racket.Top&&ball.Bottom<=racket.Bottom&&ball.Left>=racket.Left&&ball.Right<=racket.Right)
+            {
+                speed_top += 8;
+                speed_left += 8;
+                speed_top = -speed_top;
+                point += 4;
+            }
+
+            if (ball.Left<=panel1.Left)
+            {
+                speed_left = -speed_left;
+            }
+
+            if (ball.Right>=panel1.Right)
+            {
+                speed_left = -speed_left;
+            }
+
+            if (ball.Top<=panel1.Top)
+            {
+                speed_top = -speed_top;
+            }
+
+            if (ball.Bottom>=panel1.Bottom)
+            {
+                timer1.Enabled = false;
+            }
+        }
+    }
+}
